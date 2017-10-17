d9c3ac7b9dcfd26993de96ca75b44e2baa059615
Alexcr <alexcr.telecom@gmail.com>
2017-10-17 15:29:51 +0300

    asd

--- qpanel/model.py
+++ qpanel/model.py
@@ -166,6 +166,7 @@ def parse_list_record(list):
         time = int(list[0])
         record['time'] = utils.dt(time)
     except:
+        print(list)
         pass
 
     i = 1
@@ -205,9 +206,9 @@ def queuelog_exists_record(log):
 
     return session_db.query(
         exists().where(QueueLog.time == log['time']).
-            where(QueueLog.event == log['event']).
-            where(QueueLog.queuename == log['queuename']).
-            where(QueueLog.callid == log['callid'])
+        where(QueueLog.event == log['event']).
+        where(QueueLog.queuename == log['queuename']).
+        where(QueueLog.callid == log['callid'])
     ).scalar()
 
 
